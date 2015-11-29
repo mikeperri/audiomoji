c1b43011d78a98cfee04a6207aaa2c34320a5524
Mike Perri <me@michaeljperri.com>
2015-11-29 16:01:17 -0500

    Initial commit- app working

---
+++ Trillmoji/Trillmoji-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Trillmoji-Bridging-Header.h
+//  Trillmoji
+//
+//  Created by Michael Perri on 11/27/15.
+//  Copyright © 2015 Michael Perri. All rights reserved.
+//
+
+#ifndef Trillmoji_Bridging_Header_h
+#define Trillmoji_Bridging_Header_h
+
+#import <MBProgressHUD.h>
+
+#endif /* Trillmoji_Bridging_Header_h */
